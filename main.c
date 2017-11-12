56f2d3234bb06704963ad6a77599d9772f31d7f7
Dregassa <dregassa@stuy.edu>
2017-11-12 12:56:10 -0500

    minor fix

--- main.c
+++ main.c
@@ -4,19 +4,16 @@
 #include <stdlib.h>
 
 char ** parse_args(char * s1){
-  char ** ret = (char **) calloc (6, sizeof(char*));
-  int i = 0;
-  for (;i<5;i++){
-    *(ret + i) = strsep(&s1," "); 
-  }
-  return ret;
+	char ** ret = (char **) calloc (6, sizeof(char*));
+	int i = 0;
+	for (;i<5;i++){
+		ret[i] = strsep(&s1," "); 
+	}
+	return ret;
 }
 int main(){
-  //char * line = "ls -a -l";
-  char line[100] = "ls -a -l";
-  char * s1 = line;
-  char ** test = parse_args(s1);
-  execvp(test[0], test);
- 
-	
+	//char * line = "ls -a -l";
+	char line[100] = "ls -a -l";
+	char ** test = parse_args(line);
+	execvp(test[0], test);
 }
